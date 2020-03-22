4ea9d4de68eb334b76a0b9bbff62d15f6f0eb2e7
Miguel Cabrera <mcabrera@newyorker.de>
2020-03-22 20:00:37 +0100

    add find_first_failing to the preconditions module

--- hooqu/analyzers/preconditions.py
+++ hooqu/analyzers/preconditions.py
@@ -1,8 +1,24 @@
 """Preconditions are tested before the analysis is run"""
 
-from typing import Callable
+from typing import Callable, Sequence, Optional
 from pandas import DataFrame
 from hooqu.generic import generic_is_numeric
+
+
+def find_first_failing(
+        #  TODO: decide here if to send the full dataframe or the api
+        data: DataFrame,  # maybe dtypesx
+        conditions: Sequence[Callable[[DataFrame], None]]
+) -> Optional[Exception]:
+
+    print(conditions)
+    for cond in conditions:
+        try:
+            cond(data)
+        except Exception as e:
+            return e
+
+    return None
 
 
 def has_column(column: str) -> Callable[[DataFrame], None]:
