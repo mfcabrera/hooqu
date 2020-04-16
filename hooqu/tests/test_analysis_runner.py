98c0a6784126f5b2b7122b77f066eebf6344941d
Miguel Cabrera <mcabrera@newyorker.de>
2020-04-16 15:01:32 +0200

    mo' test to analysis runner

--- hooqu/tests/test_analysis_runner.py
+++ hooqu/tests/test_analysis_runner.py
@@ -1,12 +1,20 @@
 import pandas as pd
 from pandas.testing import assert_frame_equal
-from hooqu.analyzers.runners.analysis_runner import do_analysis_run, AnalyzerContext
-from hooqu.analyzers import Minimum, Size, Completeness
+from tryingsnake import Success
+
+
+from hooqu.analyzers import Completeness, Maximum, Mean, Minimum, Size
+from hooqu.analyzers.runners.analysis_runner import AnalyzerContext, do_analysis_run
+from hooqu.metrics import DoubleMetric, Entity
 
 
 class TestAnalysis:
     def test_return_result_for_configured_analyzers(self, df_full):
-        analyzers = [Size(), Minimum("item"), Completeness("item")]
+        analyzers = [
+            Size(),
+            Minimum("item"),
+            Completeness("item"),
+        ]
 
         ac = do_analysis_run(df_full, analyzers)
 
@@ -14,9 +22,11 @@ class TestAnalysis:
         print(sm)
 
         expected = pd.DataFrame(
-            [("DATASET", "*", "Size", 4.0),
-             ("COLUMN", "item", "Minimum", 1.0),
-             ("COLUMN", "item", "Completeness", 1.0)],
+            [
+                ("DATASET", "*", "Size", 4.0),
+                ("COLUMN", "item", "Minimum", 1.0),
+                ("COLUMN", "item", "Completeness", 1.0),
+            ],
             columns=("entity", "instance", "name", "value"),
         )
 
@@ -35,3 +45,31 @@ class TestAnalysis:
         metric = ac.metric(Size())
         assert metric is not None
         assert metric.value.get() == 4
+
+    def test_return_basic_statistics(self, df_with_numeric_values):
+        df = df_with_numeric_values
+        analyzers = [
+            Mean("att1"),
+            # TODO: StandardDeviation("att1")
+            Minimum("att1"),
+            Maximum("att1"),
+            # ApproxQuantile("att1", 0.5)
+            # ApproxCountDistinct("att1")
+            # CountDistinct("att1")
+        ]
+
+        result_metrics = do_analysis_run(df, analyzers).all_metrics()
+
+        assert len(result_metrics) == len(analyzers)
+
+        assert (
+            DoubleMetric(Entity.COLUMN, "Mean", "att1", Success(3.5)) in result_metrics
+        )
+        assert (
+            DoubleMetric(Entity.COLUMN, "Minimum", "att1", Success(1.0))
+            in result_metrics
+        )
+        assert (
+            DoubleMetric(Entity.COLUMN, "Maximum", "att1", Success(6.0))
+            in result_metrics
+        )
