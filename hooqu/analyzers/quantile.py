a48594882949eb4e5bf34be078a711f65d285fde
Miguel Cabrera <mcabrera@newyorker.de>
2020-05-03 12:26:14 +0200

    forgot to add the actual quantile module

---
+++ hooqu/analyzers/quantile.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from typing import Callable, List, Optional
+
+from hooqu.analyzers.analyzer import (AggDefinition, DoubledValuedState,
+                                      StandardScanShareableAnalyzer)
+from hooqu.analyzers.preconditions import has_column, is_numeric
+from hooqu.dataframe import DataFrame, quantile_aggregation
+
+
+@dataclass
+class QuantileState(DoubledValuedState):
+
+    min_value: float
+
+    def sum(self, other: "QuantileState"):
+        return min(self.min_value, other.min_value)
+
+    def metric_value(self):
+        return self.min_value
+
+
+class Quantile(StandardScanShareableAnalyzer[QuantileState]):
+    """
+    Quantile analyzer that computes the quantile using a linear interpolation, i.e.
+    returning a value within the column.
+
+    Parameters:
+    -----------
+
+    column:
+        Column in DataFrame for which the quantile is analyzed.
+
+    quantile:
+        Computed Quantile. Must be in the interval [0, 1], where 0.5 would be thee
+        median.
+
+    where:
+         Additional filter to apply before the analyzer is run.
+
+    """
+    def __init__(self, column: str, quantile: float, where: Optional[str] = None):
+        super().__init__("Quantile", column, where=where)
+        self.quantile = quantile
+
+    def from_aggregation_result(
+        self, result: DataFrame, offset: int = 0
+    ) -> Optional[QuantileState]:
+        value = 0
+        if len(result):  # otherwise an empty dataframe
+            value = result.loc["quantile_aggregation"][self.instance]
+
+        return QuantileState(value)
+
+    def _aggregation_functions(self, where: Optional[str] = None) -> AggDefinition:
+
+        return {self.instance: [quantile_aggregation(self.quantile)]}
+
+    def additional_preconditions(self) -> List[Callable[[DataFrame], None]]:
+        return [has_column(self.instance), is_numeric(self.instance)]
