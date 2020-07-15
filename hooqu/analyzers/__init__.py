fb2b2ae816fa339bbfb43167eadf3c6204e1a977
Miguel Cabrera <mcabrera@newyorker.de>
2020-07-15 17:39:41 +0200

    Added uniqueness constraint

--- hooqu/analyzers/__init__.py
+++ hooqu/analyzers/__init__.py
@@ -6,6 +6,7 @@ from hooqu.analyzers.analyzer import (
 )
 from hooqu.analyzers.completeness import Completeness
 from hooqu.analyzers.compliance import Compliance
+from hooqu.analyzers.grouping_analyzers import FrequenciesAndNumRows
 from hooqu.analyzers.maximum import Maximum, MaxState
 from hooqu.analyzers.mean import Mean, MeanState
 from hooqu.analyzers.minimum import Minimum, MinState
@@ -13,6 +14,7 @@ from hooqu.analyzers.quantile import Quantile, QuantileState
 from hooqu.analyzers.size import NumMatches, Size
 from hooqu.analyzers.standard_deviation import StandardDeviation, StandardDeviationState
 from hooqu.analyzers.sum import Sum, SumState
+from hooqu.analyzers.uniqueness import Uniqueness
 
 __all__ = [
     "Analyzer",
@@ -35,4 +37,6 @@ __all__ = [
     "NumMatchesAndCount",
     "SumState",
     "StandardDeviationState",
+    "Uniqueness",
+    "FrequenciesAndNumRows",
 ]
--- hooqu/constraints/__init__.py
+++ hooqu/constraints/__init__.py
@@ -1,19 +1,37 @@
-from hooqu.constraints.analysis_based_constraint import (  # noqa: F401
-    AnalysisBasedConstraint,
-)
-
-from hooqu.constraints.constraint import (  # noqa: F401
+from hooqu.constraints.analysis_based_constraint import AnalysisBasedConstraint
+from hooqu.constraints.constraint import (
     Constraint,
     ConstraintDecorator,
     ConstraintResult,
     ConstraintStatus,
 )
-from hooqu.constraints.constraints import completeness_constraint  # noqa: F401
-from hooqu.constraints.constraints import max_constraint  # noqa: F401
-from hooqu.constraints.constraints import mean_constraint  # noqa: F401
-from hooqu.constraints.constraints import min_constraint  # noqa: F401
-from hooqu.constraints.constraints import size_constraint  # noqa: F401
-from hooqu.constraints.constraints import standard_deviation_constraint  # noqa: F401
-from hooqu.constraints.constraints import sum_constraint  # noqa: F401
-from hooqu.constraints.constraints import quantile_constraint  # noqa: F401
-from hooqu.constraints.constraints import compliance_constraint  # noqa: F401
+from hooqu.constraints.constraints import (
+    completeness_constraint,
+    compliance_constraint,
+    max_constraint,
+    mean_constraint,
+    min_constraint,
+    quantile_constraint,
+    size_constraint,
+    standard_deviation_constraint,
+    sum_constraint,
+    uniqueness_constraint,
+)
+
+__all__ = [
+    "completeness_constraint",
+    "max_constraint",
+    "mean_constraint",
+    "min_constraint",
+    "size_constraint",
+    "standard_deviation_constraint",
+    "sum_constraint",
+    "quantile_constraint",
+    "uniqueness_constraint",
+    "compliance_constraint",
+    "AnalysisBasedConstraint",
+    "Constraint",
+    "ConstraintDecorator",
+    "ConstraintResult",
+    "ConstraintStatus",
+]
